453830c20ed6f28e13c7c525880c62232805396a
dylanpimont18-hub <dylan.pimont18@gmail.com>
2026-06-22 14:53:18 +0200

    feat: add Firebase SDK and remove maintenance redirect

---
+++ js/auth/firebase-config.js
@@ -0,0 +1,13 @@
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDRhvBYxZCo26AALEpY7s2P0vsD8YXC4DM",
+  authDomain: "spark-learning-7d96b.firebaseapp.com",
+  projectId: "spark-learning-7d96b",
+  storageBucket: "spark-learning-7d96b.firebasestorage.app",
+  messagingSenderId: "94424829172",
+  appId: "1:94424829172:web:be571db12ebf726a8d62a1"
+};
+
+// Initialiser Firebase si pas déjà fait
+if (!firebase.apps.length) {
+  firebase.initializeApp(FIREBASE_CONFIG);
+}
